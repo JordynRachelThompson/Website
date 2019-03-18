e7463ad6865a12ee35a2b6b46d65ca1e2dc0d5d8
JordynRachelThompson <jordynrachelthompson@gmail.com>
2019-03-17 20:03:00 -0400

    changes

--- MyWebsite/Controllers/HomeController.cs
+++ MyWebsite/Controllers/HomeController.cs
@@ -11,7 +11,11 @@ namespace MyWebsite.Controllers
             ViewBag.UseHomePageLinks = true;
 
             ViewBag.BudgetAppDescr = "The Painless Budget is a user-friendly, responsive budgeting app that " +
-                "lets registered users create a monthly budget, view spending insights and analytics, export their budget history to Excel and more.";      
+                "lets registered users create a monthly budget, view spending insights and analytics, export their budget history to Excel and more.";
+
+            ViewBag.WeatherAppDescr = "Open Weather Elemet Alerts lets users track weather via Open WeatherMap's API and view a 5 day, hourly forecast for their selected " +
+                "city. Unregistered users can also view forecasts. Once registered, users can set their city and manage element alerts that will " +
+                "alert them when a particular weather condition is present in today's forecast.";
 
             return View();
         }
